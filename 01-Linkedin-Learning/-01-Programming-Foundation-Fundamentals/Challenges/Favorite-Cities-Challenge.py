02f0d4c348457954b811205963be9bb649fee633
mohamedanwar8 <84838270+mohamedanwar8@users.noreply.github.com>
2022-07-15 06:13:00 +0200

    Update Favorite-Cities-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Favorite-Cities-Challenge.py
@@ -1,1 +1,8 @@
 
+# print the name of my favourite cities using at least one function
+def favorite_city(name):
+    print("One of my favourite sityies is", name)
+
+favorite_city("Cairo")
+favorite_city("Giza")
+favorite_city("Menuf")
